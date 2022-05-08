7c9d1713ce554a955ad7542809c048adf6b31803
Sungmin Ashley Ro <sungminro92@gmail.com>
2022-05-08 01:20:45 -0400

    on CONFIGURING STORE SETUP

---
+++ src/configureStore.js
@@ -0,0 +1,20 @@
+import { applyMiddleware, createStore } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+
+import monitorReducersEnhancer from './enhancers/monitorReducers'
+import loggerMiddleware from './middleware/logger'
+import rootReducer from './shopReducers'
+
+export default function configureStore(preloadedState) {
+    const middlewares = [loggerMiddleware, thunkMiddleware]
+    const middlewareEnhancer = applyMiddleware(...middlewares)
+
+    const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
+    const composedEnhancers = composeWithDevTools(...enhancers)
+
+    const store = createStore(rootReducer, preloadedState, composedEnhancers)
+
+    return store
+}
---
+++ src/shopReducers.js
@@ -0,0 +1,7 @@
+const shopReducer = (state, action) => {
+    switch (action.type) {
+
+    }
+
+};
+export default shopReducer;
